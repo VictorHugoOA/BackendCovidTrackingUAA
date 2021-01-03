dca3a1116814a08b79de3e7114caf84de698c681
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2021-01-03 15:32:37 -0600

    Actualizar área

--- Servidor NodeJS/routes/actualizar.js
+++ Servidor NodeJS/routes/actualizar.js
@@ -4,12 +4,22 @@ function createRoute(db){
     const router = express.Router();
 
     /*Mostrar area por ID*/
-    router.get('/area/:Id', function(req, res){
+    router.post('/area', function(req, res){
 
-        const id = req.params.Id;
+        const id = req.body.Id;
+        const id_org = req.body.Id_Organizacion;
+        const nombre = req.body.Nombre;
+        const desc = req.body.Descripcion;
+        const edificio = req.body.Edificio;
+        const piso = req.body.Piso;
+        const QR = req.body.QR;
+        const Riesgo = req.body.Riesgo;
+        const fechaRojo = req.body.FechaRojo;
+        const latitud = req.body.Latitud;
+        const longitud = req.body.Longitud;
 
-        db.query('select * from area where Id = ?',
-         [id], (error, results) =>{
+        db.query('update area set Id_Organizacion = ?, Nombre = ?, Descripcion = ?, Edificio = ?, Piso = ?, QR = ?, Riesgo = ?, Fecha_Rojo = ?, Latitud = ?, Longitud = ? where Id = ?',
+         [id_org, nombre, desc, edificio, piso, QR, Riesgo, fechaRojo, latitud, longitud, id], (error, results) =>{
             if(error) {return res.status(501).json({error: error})}
             res.send(results);
             console.log(results);
