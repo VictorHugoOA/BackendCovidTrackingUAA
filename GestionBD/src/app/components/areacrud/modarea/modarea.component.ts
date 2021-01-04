4cb065493f95e911532e5c979f97f4253b99a197
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2021-01-04 03:29:24 -0600

    Actualizar área y usuario

--- GestionBD/src/app/components/areacrud/modarea/modarea.component.ts
+++ GestionBD/src/app/components/areacrud/modarea/modarea.component.ts
@@ -1,6 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { AreaService } from 'src/app/services/area/area.service';
+import { OrgService } from 'src/app/services/organizacion/org.service';
 
 @Component({
   selector: 'app-modarea',
@@ -9,19 +14,30 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ModareaComponent implements OnInit {
   AltaArea: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  id: FormControl;
+  organizations: any[] = [];
+  constructor(private fb: FormBuilder, private toastr: ToastrService, private area: AreaService, private org: OrgService) {
+    this.id = new FormControl('', Validators.required);
+    this.org.mostrarOrganizaciones().subscribe((data: any[]) => {
+      for (let i = 0; i < data.length; ++i) {
+        this.organizations.push({ id: data[i].Id, nombre: data[i].Nombre });
+      }
+    })
+  }
+
+  areaC: Observable<any>;
 
   ngOnInit(): void {
     this.AltaArea = this.fb.group({
 
       id: ['', Validators.required],
       idOrg: ['', Validators.required],
-      name: ['', Validators.required],
-      desc: ['', Validators.required],
-      edi: ['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(10), Validators.maxLength(10)]],
+      name: ['', [Validators.required, Validators.maxLength(200)]],
+      desc: ['', [Validators.required, Validators.maxLength(600)]],
+      edi: ['', Validators.maxLength(100)],
       piso: ['', Validators.required],
-      riesgo: ['', Validators.required],
-      fecha: ['', Validators.required],
+      riesgo: ['', [Validators.required, Validators.maxLength(30)]],
+      fecha: [null],
       latitud: ['', Validators.required],
       longitud: ['', Validators.required],
 
@@ -29,9 +45,41 @@ export class ModareaComponent implements OnInit {
 
   }
   onSubmit() {
-
+    if (this.AltaArea.valid) {
+      this.area.actualizarArea(
+        this.AltaArea.get('id').value,
+        this.AltaArea.get('idOrg').value,
+        this.AltaArea.get('name').value,
+        this.AltaArea.get('desc').value,
+        this.AltaArea.get('edi').value,
+        this.AltaArea.get('piso').value,
+        null,
+        this.AltaArea.get('riesgo').value,
+        this.AltaArea.get('fecha').value,
+        this.AltaArea.get('latitud').value,
+        this.AltaArea.get('longitud').value,
+      ).subscribe((result) => { }, (error) => { this.toastr.error("Ocurrió un error. Intenta cambiando el id del área o vuelve a intentar", "Error") });
+    }
   }
-  Buscar(area) {
-
+  Buscar() {
+    if (this.id.valid) {
+      this.areaC = this.area.mostarArea(this.id.value);
+      this.areaC.subscribe((data: any) => {
+        this.AltaArea.setValue({
+          id: data.Id,
+          idOrg: data.Id_Organizacion,
+          name: data.Nombre,
+          desc: data.Descripcion,
+          edi: data.Edificio,
+          piso: data.Piso,
+          riesgo: data.Riesgo,
+          fecha: (new Date(data.Fecha_Rojo)),
+          latitud: data.Latitud,
+          longitud: data.Longitud
+        })
+      })
+    } else {
+      this.toastr.warning("Por favor, ingresa datos para buscar", "No hay datos");
+    }
   }
 }
--- GestionBD/src/app/components/usercrud/moduser/moduser.component.ts
+++ GestionBD/src/app/components/usercrud/moduser/moduser.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/usuario/user.service';
 
 @Component({
   selector: 'app-moduser',
@@ -7,26 +10,76 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./moduser.component.css']
 })
 export class ModuserComponent implements OnInit {
-AltaUser: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  AltaUser: FormGroup;
+  uu: FormControl;
+  usuario: Observable<any>
+  constructor(private fb: FormBuilder, private persona: UserService, private toastr: ToastrService) {
+    this.uu = new FormControl('', Validators.required)
+  }
 
   ngOnInit(): void {
     this.AltaUser = this.fb.group({
-      user: ['', Validators.required],
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      pass: ['', Validators.required],
-      riesgo: ['', Validators.required],
-      fecha: ['', Validators.required],
+      user: ['', [Validators.required, Validators.maxLength(100)]],
+      name: ['', [Validators.required, Validators.maxLength(300)]],
+      email: ['', [Validators.required, Validators.maxLength(200), Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      pass: ['', [Validators.required, Validators.maxLength(32)]],
+      auth: ['', Validators.required],
+      riesgo: ['', [Validators.required, Validators.maxLength(30)]],
+      fecha: [null],
+      tipouser: ['', Validators.required]
     });
-    
+
   }
 
   onSubmit() {
-    
-}
-Buscar(user){
+    let autF = 0;
+    let autImei = 0;
+    if (this.AltaUser.valid) {
+      if (this.AltaUser.get('auth').value == "Facebook") {
+        autF = 1
+      }
+      else if (this.AltaUser.get('auth').value == "IMEI") {
+        autImei = 1
+      }
+      this.persona.actualizarUsuario(
+        this.AltaUser.get('user').value,
+        this.AltaUser.get('name').value,
+        this.AltaUser.get('email').value,
+        this.AltaUser.get('pass').value,
+        autF,
+        autImei,
+        this.AltaUser.get('riesgo').value,
+        this.AltaUser.get('fecha').value,
+        this.AltaUser.get('tipouser').value
 
-}
+      ).subscribe((result) => {}, (error) => {this.toastr.error("Ocurrió un error. Intenta cambiando el nombre de usuario o vuelve a intentar", "Error")});
+    }
+  }
+  Buscar() {
+    console.log(this.uu);
+    if (this.uu.valid) {
+      this.usuario = this.persona.mostrarUsuario(this.uu.value);
+      this.usuario.subscribe((data: any) => {
+        let aut = "";
+        if(data.AutFacebook == 1){
+          aut = "Facebook"
+        }else if(data.IMEI){
+          aut = "IMEI"
+        }
+        this.AltaUser.setValue({
+          user: data.Usuario,
+          name: data.Nombre,
+          email: data.Correo,
+          pass: data.Contrasena,
+          auth: aut,
+          riesgo: data.Riesgo,
+          fecha: (new Date(data.Fecha_Rojo)),
+          tipouser: data.Admin
+        })
+      })
+    } else {
+      this.toastr.warning("Por favor, ingresa datos para buscar", "No hay datos");
+    }
+  }
 
 }
