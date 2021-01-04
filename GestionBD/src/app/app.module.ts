56be260df336192725d2c0a08aa38aca208b496b
NRekits <64814187+NRekits@users.noreply.github.com>
2021-01-03 18:37:03 -0600

    Borrar componentes

--- GestionBD/src/app/app.module.ts
+++ GestionBD/src/app/app.module.ts
@@ -2,12 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
-import { TemplateformsComponent } from './templateforms/templateforms.component';
 import { FormsModule } from '@angular/forms'
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
-import { VisitaformComponent } from './visitaform/visitaform.component';
-import { OrganizacionformComponent } from './organizacionform/organizacionform.component';
 import { AreaformComponent } from './areaform/areaform.component';
 import { QrComponent } from './components/qr/qr.component';
 import { QRCodeModule } from 'angularx-qrcode';
@@ -28,10 +24,6 @@ import { BuscaruserComponent } from './components/usercrud/buscaruser/buscaruser
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveformsComponent,
-    TemplateformsComponent,
-    VisitaformComponent,
-    OrganizacionformComponent,
     AreaformComponent,
     QrComponent,
     NavMenuComponent,
