fd6a7d892e58ef26049143cd1b54375e7b75016c
NRekits <64814187+NRekits@users.noreply.github.com>
2021-01-04 01:51:23 -0600

    ver completo

--- GestionBD/src/app/components/usercrud/veruser/veruser.component.ts
+++ GestionBD/src/app/components/usercrud/veruser/veruser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/usuario/user.service';
 
 @Component({
   selector: 'app-veruser',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./veruser.component.css']
 })
 export class VeruserComponent implements OnInit {
-
-  constructor() { }
+  usuarios:any[]=[];
+  constructor(private users:UserService) { }
 
   ngOnInit(): void {
+    this.users.mostrarUsuarios().subscribe((data:any[]) =>
+    {
+      for(var i =0; i<data.length;i++){
+        this.usuarios.push(data[i]);
+      }
+    }
+  );
   }
 
 }
+
